d789b74c2824c751dc183ec0540a03759d1c3c09
Joell <amamihechi.udu@decagon.dev>
2020-02-14 14:01:26 +0100

    fixed register page

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -70,6 +70,9 @@ namespace DelishWebsite.Models
         public string Email { get; set; }
 
         [Required]
+        public string Name { get; set; }
+
+        [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
