ae90017edbc24c7cb338f9b09820dfc82e91b5b5
Joell <amamihechi.udu@decagon.dev>
2020-02-15 10:48:26 +0100

    Added cart, special request section

---
+++ Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace DelishWebsite.Controllers
+{
+    public class OrderController : Controller
+    {
+        // GET
+        public ActionResult Cart()
+        {
+            return View();
+        }
+    }
+}
